830e11c28bea9cb25438e24e66b26d82cd2886f4
alexjun12 <alexjun12@naver.com>
2021-12-21 23:47:36 +0900

    vac_python3

--- Anti_Cho/Vac/antivirus.py
+++ Anti_Cho/Vac/antivirus.py
@@ -1,13 +1,45 @@
 import sys
 import os
 import hashlib
+import zlib
+import io
 
 VirusDB = [] #파일크기:MD5 해시:악성코드 이름
 vdb = [] #가공된 악성코드
 vsize = [] #악성코드 파일크기 저장
 
+def DecodeKMD(fname) : #KMD파일 복호화
+    try :
+        fp = open(fname, 'rb')
+        buf = fp.read()
+        fp.close()
+
+        buf2 = buf[:-32] #암호화 내용 분리
+        fmd5 = buf[-32:] #md5 분리
+
+        f = buf2
+        for i in range(3) :
+            md5 = hashlib.md5()
+            md5.update(f)
+            f = md5.hexdigest()
+
+        if f != fmd5 :
+            raise SystemError
+
+        buf3 = ''
+        for c in buf2[4:] : # 0xFF로 XOR한다
+            buf3 += chr(ord(c) ^ 0xFF)
+
+        buf4 = zlib.decompress(buf3) # 압축 해제
+        return buf4 #성공했다면 복호화된 내용 반환
+    except :
+        pass
+
+    return None #오류시 None반환
+
 def LoadVirusDB() : #virus.db에서 악성코드 패턴 읽기
-    fp = open('virus.db', 'rb')
+    buf = DecodeKMD('virus.kmd')
+    fp = io.StringIO(buf)
 
     while(True) :
         line = fp.readline()
